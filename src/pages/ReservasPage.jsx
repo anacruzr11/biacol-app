ec05689565ce59be50a0bd30d33cd5057095867e
Ana Cruz <anmacruro4@gmail.com>
2023-07-21 19:13:15 -0500

    first commit

---
+++ src/pages/ReservasPage.jsx
@@ -0,0 +1,16 @@
+const ReservasPage = () => {
+  return (
+    <>
+      <header className="row col">
+        <h1>Reservas Page</h1>
+      </header>
+      <main className="row">
+        <article className="col">
+          <p>Contenido de la pagina acerca de</p>
+        </article>
+      </main>
+    </>
+  );
+};
+
+export default ReservasPage;
